9bfb70150debbf5c8c565dc70ea39ea68c68e661
Artur Czeczko <a.czeczko@icm.edu.pl>
2013-06-06 12:47:35 +0200

    Correct directories separator

--- src/main/java/pl/edu/icm/maven/oozie/plugin/AbstractIntegrationTestMojo.java
+++ src/main/java/pl/edu/icm/maven/oozie/plugin/AbstractIntegrationTestMojo.java
@@ -19,141 +19,135 @@ import org.springframework.util.PropertyPlaceholderHelper;
 
 public abstract class AbstractIntegrationTestMojo extends AbstractOozieMojo {
 
-	protected URI hdfsURI;
+    protected URI hdfsURI;
+    protected String hdfsWfWorkingDir;
+    protected String hdfsUserName;
+    protected String wfDir;
+    protected FileSystem hdfsFS;
 
-	protected String hdfsWfWorkingDir;
+    @Override
+    public void execute() throws MojoExecutionException {
+        super.execute();
 
-	protected String hdfsUserName;
+        initializeFieldsUsingEnvPropertiesFile();
+        createHdfsFS();
+    }
 
-	protected String wfDir;
+    protected void initializeFieldsUsingEnvPropertiesFile()
+            throws MojoExecutionException {
 
-	protected FileSystem hdfsFS;
+        String itEnvPropertiesLocation = OoziePluginConstants.IT_ENV_PROPERTIES_LOCATION.replaceAll(
+                Pattern.quote(OoziePluginConstants.IT_ENV_PLACEHOLDER),
+                envIT);
 
-	@Override
-	public void execute() throws MojoExecutionException {
-		super.execute();
+        Properties envConf = loadPropertiesFromTestResources(itEnvPropertiesLocation);
+        resolvePlaceholders(envConf);
 
-		initializeFieldsUsingEnvPropertiesFile();
-		createHdfsFS();
-	}
+        hdfsUserName = envConf.getProperty(OoziePluginConstants.HDFS_USER_NAME);
+        if (hdfsUserName != null) {
+            System.setProperty("HADOOP_USER_NAME", hdfsUserName);
+        }
 
-	protected void initializeFieldsUsingEnvPropertiesFile()
-			throws MojoExecutionException {
+        String hdfsURIName = envConf.getProperty(OoziePluginConstants.HDFS_URI);
+        try {
+            hdfsURI = new URI(hdfsURIName);
+        } catch (URISyntaxException e) {
+            throw new MojoExecutionException("HDFS URI cannot be parsed.", e);
+        }
 
-		String itEnvPropertiesLocation = OoziePluginConstants.IT_ENV_PROPERTIES_LOCATION
-				.replaceAll(
-						Pattern.quote(OoziePluginConstants.IT_ENV_PLACEHOLDER),
-						envIT);
+        hdfsWfWorkingDir = envConf.getProperty(OoziePluginConstants.HDFS_WF_WORKING_DIR);
+        if (hdfsWfWorkingDir == null) {
+            throw new MojoExecutionException("Property "
+                    + OoziePluginConstants.HDFS_WF_WORKING_DIR
+                    + " cannot be empty.");
+        }
 
-		Properties envConf = loadPropertiesFromTestResources(itEnvPropertiesLocation);
-		resolvePlaceholders(envConf);
+        wfDir = envConf.getProperty(OoziePluginConstants.WF_DIR);
+        if (wfDir == null) {
+            throw new MojoExecutionException("Property "
+                    + OoziePluginConstants.WF_DIR + " cannot be empty.");
+        }
+    }
 
-		hdfsUserName = envConf.getProperty(OoziePluginConstants.HDFS_USER_NAME);
-		if (hdfsUserName != null) {
-			System.setProperty("HADOOP_USER_NAME", hdfsUserName);
-		}
+    private void createHdfsFS() throws MojoExecutionException {
+        Configuration hdfsFSconf = new Configuration();
+        hdfsFSconf.set("fs.hdfs.impl",
+                "org.apache.hadoop.hdfs.DistributedFileSystem");
+        try {
+            hdfsFS = FileSystem.get(hdfsURI, hdfsFSconf);
+        } catch (IOException e) {
+            throw new MojoExecutionException("HDFS FileSystem with URI "
+                    + hdfsURI.toString() + " failed to be created.", e);
+        }
+    }
 
-		String hdfsURIName = envConf.getProperty(OoziePluginConstants.HDFS_URI);
-		try {
-			hdfsURI = new URI(hdfsURIName);
-		} catch (URISyntaxException e) {
-			throw new MojoExecutionException("HDFS URI cannot be parsed.", e);
-		}
+    protected void resolvePlaceholders(Properties properties) {
+        String placeholderPrefix = properties.getProperty(
+                OoziePluginConstants.PLACEHOLDER_PREFIX_NAME,
+                OoziePluginConstants.PLACEHOLDER_PREFIX_DEFAULT);
+        String placeholderSuffix = properties.getProperty(
+                OoziePluginConstants.PLACEHOLDER_SUFFIX_NAME,
+                OoziePluginConstants.PLACEHOLDER_SUFFIX_DEFAULT);
+        String placeholderValuesSeparator = properties.getProperty(
+                OoziePluginConstants.PLACEHOLDER_VALUE_SEPARATOR_NAME,
+                OoziePluginConstants.PLACEHOLDER_VALUE_SEPARATOR_DEFAULT);
 
-		hdfsWfWorkingDir = envConf
-				.getProperty(OoziePluginConstants.HDFS_WF_WORKING_DIR);
-		if (hdfsWfWorkingDir == null) {
-			throw new MojoExecutionException("Property "
-					+ OoziePluginConstants.HDFS_WF_WORKING_DIR
-					+ " cannot be empty.");
-		}
+        boolean ignoreUnresolvablePlaceholders = Boolean.parseBoolean(properties.getProperty(
+                OoziePluginConstants.PLACEHOLDER_IGNORE_UNRESOLVABLE_PLACEHOLDERS_NAME,
+                OoziePluginConstants.PLACEHOLDER_IGNORE_UNRESOLVABLE_PLACEHOLDERS_DEFAULT));
 
-		wfDir = envConf.getProperty(OoziePluginConstants.WF_DIR);
-		if (wfDir == null) {
-			throw new MojoExecutionException("Property "
-					+ OoziePluginConstants.WF_DIR + " cannot be empty.");
-		}
-	}
+        PropertyPlaceholderHelper pph = new PropertyPlaceholderHelper(
+                placeholderPrefix, placeholderSuffix,
+                placeholderValuesSeparator, ignoreUnresolvablePlaceholders);
 
-	private void createHdfsFS() throws MojoExecutionException {
-		Configuration hdfsFSconf = new Configuration();
-		hdfsFSconf.set("fs.hdfs.impl",
-				"org.apache.hadoop.hdfs.DistributedFileSystem");
-		try {
-			hdfsFS = FileSystem.get(hdfsURI, hdfsFSconf);
-		} catch (IOException e) {
-			throw new MojoExecutionException("HDFS FileSystem with URI "
-					+ hdfsURI.toString() + " failed to be created.", e);
-		}
-	}
+        for (String propertyName : properties.stringPropertyNames()) {
+            properties.setProperty(
+                    propertyName,
+                    pph.replacePlaceholders(
+                    properties.getProperty(propertyName), properties));
+        }
+    }
 
-	protected void resolvePlaceholders(Properties properties) {
-		String placeholderPrefix = properties.getProperty(
-				OoziePluginConstants.PLACEHOLDER_PREFIX_NAME,
-				OoziePluginConstants.PLACEHOLDER_PREFIX_DEFAULT);
-		String placeholderSuffix = properties.getProperty(
-				OoziePluginConstants.PLACEHOLDER_SUFFIX_NAME,
-				OoziePluginConstants.PLACEHOLDER_SUFFIX_DEFAULT);
-		String placeholderValuesSeparator = properties.getProperty(
-				OoziePluginConstants.PLACEHOLDER_VALUE_SEPARATOR_NAME,
-				OoziePluginConstants.PLACEHOLDER_VALUE_SEPARATOR_DEFAULT);
+    protected Properties loadPropertiesFromTestResources(
+            String propertiesFileLocation) throws MojoExecutionException {
 
-		boolean ignoreUnresolvablePlaceholders = Boolean
-				.parseBoolean(properties
-						.getProperty(
-								OoziePluginConstants.PLACEHOLDER_IGNORE_UNRESOLVABLE_PLACEHOLDERS_NAME,
-								OoziePluginConstants.PLACEHOLDER_IGNORE_UNRESOLVABLE_PLACEHOLDERS_DEFAULT));
+        propertiesFileLocation = new File(propertiesFileLocation).getPath();
 
-		PropertyPlaceholderHelper pph = new PropertyPlaceholderHelper(
-				placeholderPrefix, placeholderSuffix,
-				placeholderValuesSeparator, ignoreUnresolvablePlaceholders);
+        List<Resource> testResourcesDirs = mavenProject.getTestResources();
+        File propertiesFile = null;
 
-		for (String propertyName : properties.stringPropertyNames()) {
-			properties.setProperty(
-					propertyName,
-					pph.replacePlaceholders(
-							properties.getProperty(propertyName), properties));
-		}
-	}
+        outerloop: for (Resource testResource : testResourcesDirs) {
+            File testResourceDir = new File(testResource.getDirectory());
+            Collection<File> testResourceFiles = FileUtils.listFiles(
+                    testResourceDir, null, true);
+            for (File testResourceFile : testResourceFiles) {
+                if (testResourceFile.getAbsolutePath().contains(
+                        propertiesFileLocation)) {
+                    propertiesFile = testResourceFile;
+                    break outerloop;
+                }
+            }
+        }
 
-	protected Properties loadPropertiesFromTestResources(
-			String propertiesFileLocation) throws MojoExecutionException {
-		List<Resource> testResourcesDirs = mavenProject.getTestResources();
-		File propertiesFile = null;
+        Properties properties = new Properties();
+        FileReader fileReader = null;
 
-		outerloop: for (Resource testResource : testResourcesDirs) {
-			File testResourceDir = new File(testResource.getDirectory());
-			Collection<File> testResourceFiles = FileUtils.listFiles(
-					testResourceDir, null, true);
-			for (File testResourceFile : testResourceFiles) {
-				if (testResourceFile.getAbsolutePath().contains(
-						propertiesFileLocation)) {
-					propertiesFile = testResourceFile;
-					break outerloop;
-				}
-			}
-		}
-
-		Properties properties = new Properties();
-		FileReader fileReader = null;
-
-		try {
-			fileReader = new FileReader(propertiesFile);
-			properties.load(fileReader);
-		} catch (Exception e) {
-			throw new MojoExecutionException("Environement properties file "
-					+ propertiesFileLocation + " not found in test resources.");
-		} finally {
-			try {
-				if (fileReader != null) {
-					fileReader.close();
-				}
-			} catch (IOException e) {
-				// FileReader has not been closed - not a big deal ;)
-				e.printStackTrace();
-			}
-		}
-		return properties;
-	}
-
+        try {
+            fileReader = new FileReader(propertiesFile);
+            properties.load(fileReader);
+        } catch (Exception e) {
+            throw new MojoExecutionException("Environement properties file "
+                    + propertiesFileLocation + " not found in test resources.");
+        } finally {
+            try {
+                if (fileReader != null) {
+                    fileReader.close();
+                }
+            } catch (IOException e) {
+                // FileReader has not been closed - not a big deal ;)
+                e.printStackTrace();
+            }
+        }
+        return properties;
+    }
 }
