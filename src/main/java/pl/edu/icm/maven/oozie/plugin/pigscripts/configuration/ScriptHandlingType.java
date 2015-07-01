3c18bfbc0a04e5a580c44f1870d7ed7e6faff34d
Artur Czeczko <a.czeczko@icm.edu.pl>
2015-07-01 11:13:47 +0200

    Changed > to &gt; in javadoc

--- src/main/java/pl/edu/icm/maven/oozie/plugin/pigscripts/configuration/ScriptHandlingType.java
+++ src/main/java/pl/edu/icm/maven/oozie/plugin/pigscripts/configuration/ScriptHandlingType.java
@@ -20,21 +20,21 @@ import javax.xml.bind.annotation.XmlType;
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="scriptHandlingType">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="srcProject" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *         &lt;element name="root" type="{http://www.w3.org/2001/XMLSchema}string"/>
- *         &lt;element name="preserve" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
- *         &lt;element name="target" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *         &lt;element name="mainDirAsDst" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
- *         &lt;element name="includes" type="{}includesType"/>
- *         &lt;element name="excludes" type="{}excludesType" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
+ * &lt;complexType name="scriptHandlingType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="srcProject" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="root" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="preserve" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/&gt;
+ *         &lt;element name="target" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="mainDirAsDst" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/&gt;
+ *         &lt;element name="includes" type="{}includesType"/&gt;
+ *         &lt;element name="excludes" type="{}excludesType" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
  * </pre>
  * 
  * 
